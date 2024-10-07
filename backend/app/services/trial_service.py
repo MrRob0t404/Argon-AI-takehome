76beb7af0fa2d01c262c82149a7b85fde9c3d226
Simon Gaviria <simongaviria1@gmail.com>
2024-10-07 15:45:14 -0400

    add date and range logic for quering trials

--- backend/app/services/trial_service.py
+++ backend/app/services/trial_service.py
@@ -1,14 +1,16 @@
-from utils.data_loader import load_trials_data
+from app.utils.data_loader import load_trials_data
+from datetime import datetime
 
-# Load the clinical trials dataset into memory
 clinical_trials = load_trials_data()
 
-def search_trials(company=None, phase=None, status=None, condition="Non-Small Cell Lung Cancer"):
+def search_trials(company=None, phase=None, status=None, condition="Non-Small Cell Lung Cancer", start_date=None, end_date=None):
     results = [
         trial for trial in clinical_trials
         if (not company or company.lower() in trial['company'].lower()) and
            (not phase or phase.lower() == trial['phase'].lower()) and
-           (not status or status.lower() == trial['status'].lower()) 
-        #    (condition.lower() in trial['condition'].lower())
+           (not status or status.lower() == trial['status'].lower()) and
+           (condition.lower() in trial['condition'].lower()) and
+           (not start_date or datetime.strptime(trial['start_date'], "%Y-%m-%d").date() >= start_date) and
+           (not end_date or datetime.strptime(trial['end_date'], "%Y-%m-%d").date() <= end_date)
     ]
     return {"results": results}

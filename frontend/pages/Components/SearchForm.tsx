db10c84e018cc2da579c880edb7ffedea8dc9624
Simon Gaviria <simongaviria1@gmail.com>
2024-10-07 17:39:21 -0400

    add StudyCard and StudyDetails to project

--- frontend/pages/Components/SearchForm.tsx
+++ frontend/pages/Components/SearchForm.tsx
@@ -1,5 +1,26 @@
-function SearchForm() {
-  return <h1>SEARCH FORM COMPONENT</h1>;
-}
+import { useState } from "react";
 
-export default SearchForm;
+export default function SearchForm({
+  onSearch,
+}: {
+  onSearch: (query: string) => void;
+}) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for a clinical trial..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}

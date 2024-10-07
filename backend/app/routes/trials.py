f6854c92a7d61195ae6ecfa2367e8e4862d3cb01
Simon Gaviria <simongaviria1@gmail.com>
2024-10-07 15:46:30 -0400

    add pagination features to trial route

--- backend/app/routes/trials.py
+++ backend/app/routes/trials.py
@@ -11,9 +11,11 @@ def get_trials(
     status: str = Query(None),
     condition: str = Query("Non-Small Cell Lung Cancer"),
     start_date: date = Query(None),
-    end_date: date = Query(None)
+    end_date: date = Query(None),
+    skip: int = Query(0),  # Skip the first `n` results
+    limit: int = Query(10)  # Limit the number of results
 ):
     if start_date and end_date and start_date > end_date:
         raise HTTPException(status_code=400, detail="start_date cannot be later than end_date")
-    
-    return search_trials(company, phase, status, condition, start_date, end_date)
+
+    return search_trials(company, phase, status, condition, start_date, end_date, skip, limit)

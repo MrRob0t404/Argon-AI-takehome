5069f4a8f9e4564114f3b546405ce2a837d25997
Simon Gaviria <simongaviria1@gmail.com>
2024-10-07 16:05:30 -0400

    add pages directory to serve to the client

---
+++ frontend/src/app/pages/index.tsx
@@ -0,0 +1,6 @@
+const SearchForm = () => {
+    return <h1>THIS IS HOME PAGE </h1>;
+  };
+  
+  export default SearchForm;
+  
---
+++ frontend/src/app/pages/results.tsx
@@ -0,0 +1,5 @@
+const SearchForm = () => {
+  return <h1>THIS IS SEACH RESULTS COMPONENT</h1>;
+};
+
+export default SearchForm;

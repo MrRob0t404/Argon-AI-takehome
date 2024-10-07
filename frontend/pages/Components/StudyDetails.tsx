f6368083bf3eb95884715c31fecb3467f11088c4
Simon Gaviria <simongaviria1@gmail.com>
2024-10-07 18:33:43 -0400

    add study details component

---
+++ frontend/pages/Components/StudyDetails.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+// TODO: Provide proper type to study
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function StudyDetails({ study }: { study: any }) {
+  const { protocolSection } = study;
+  const {
+    identificationModule,
+    statusModule,
+    descriptionModule,
+    sponsorCollaboratorsModule,
+  } = protocolSection;
+
+  return (
+    <div className="study-details">
+      <h1>DETAILS:</h1>
+      <h2>{identificationModule.officialTitle}</h2>
+      <p>
+        <strong>NCT ID:</strong> {identificationModule.nctId}
+      </p>
+      <p>
+        <strong>Status:</strong> {statusModule.overallStatus}
+      </p>
+      <p>
+        <strong>Brief Summary:</strong> {descriptionModule.briefSummary}
+      </p>
+      <p>
+        <strong>Sponsor:</strong> {sponsorCollaboratorsModule.leadSponsor.name}
+      </p>
+      {/* Add more details as necessary */}
+    </div>
+  );
+}

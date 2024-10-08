92078dcc3d2dcff4ec73c540e320fb28e1b442d2
Simon Gaviria <simongaviria1@gmail.com>
2024-10-08 12:47:01 -0400

    small update to add default behavior if the user enters an empty query

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from routes import trials  # Import trials routes
-from services.trial_service import search_trials
+from services.trial_service import search_trials, get_all_trials  # Import the new function
 
 app = FastAPI()
 
@@ -18,4 +18,8 @@ def get_trials(
     status: str = Query(None),
     condition: str = Query("Non-Small Cell Lung Cancer")
 ):
+    # Check if all search parameters are None or empty
+    if not company and not phase and not status and not condition:
+        return get_all_trials()  # Return all trials if no search criteria are provided
+
     return search_trials(company, phase, status, condition)

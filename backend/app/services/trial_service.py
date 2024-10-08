4fc69be2ae6b1ab1845bdba0f6ce3fe5fbd8acec
Simon Gaviria <simongaviria1@gmail.com>
2024-10-08 12:49:13 -0400

    introduced a helper function _is_match in trial_service.py to reduce redundancy in condition checks

--- backend/app/services/trial_service.py
+++ backend/app/services/trial_service.py
@@ -3,14 +3,22 @@ from datetime import datetime
 
 clinical_trials = load_trials_data()
 
+def _is_match(value, criteria):
+    return not criteria or criteria.lower() in value.lower()
+
 def search_trials(company=None, phase=None, status=None, condition="Non-Small Cell Lung Cancer", start_date=None, end_date=None, skip=0, limit=10):
     results = [
         trial for trial in clinical_trials
-        if (not company or company.lower() in trial['company'].lower()) and
-           (not phase or phase.lower() == trial['phase'].lower()) and
-           (not status or status.lower() == trial['status'].lower()) and
-           (condition.lower() in trial['condition'].lower()) and
-           (not start_date or datetime.strptime(trial['start_date'], "%Y-%m-%d").date() >= start_date) and
-           (not end_date or datetime.strptime(trial['end_date'], "%Y-%m-%d").date() <= end_date)
+        if (_is_match(trial['protocolSection']['identificationModule']['organization']['fullName'], company) and
+            _is_match(trial['protocolSection']['statusModule']['overallStatus'], phase) and
+            _is_match(trial['protocolSection']['statusModule']['overallStatus'], status) and
+            (trial['protocolSection'].get('conditionsModule') and trial['protocolSection']['conditionsModule'].get('conditions') and 
+             _is_match(trial['protocolSection']['conditionsModule']['conditions'][0], condition)) and
+            (not start_date or datetime.strptime(trial['protocolSection']['statusModule']['startDateStruct']['date'], "%Y-%m-%d").date() >= start_date) and
+            (not end_date or datetime.strptime(trial['protocolSection']['statusModule']['completionDateStruct']['date'], "%Y-%m-%d").date() <= end_date)
+        )
     ]
     return {"results": results[skip: skip + limit]}
+
+def get_all_trials():
+    return {"results": clinical_trials}  # Return all clinical trials

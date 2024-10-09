5d8458e5501798ecf72cc51eb37b2b03e79a7ca2
Simon Gaviria <simongaviria1@gmail.com>
2024-10-09 00:22:04 -0400

    remove logging

--- backend/app/routes/trials.py
+++ backend/app/routes/trials.py
@@ -9,9 +9,10 @@ def get_trials(
     page: int = Query(1, ge=1),
     limit: int = Query(10, ge=1, le=100)
 ):
-    # print('BANANA', len(search_trials(condition, page, limit)))
+    results = search_trials(condition, page, limit)
+    print(f'Searching trials for {condition}', len(results['results']))
     if condition:
         return search_trials(condition, page, limit)
     else:
-        # print('getting all results', len(get_all_trials(page, limit)))
+        print('getting all results', len(get_all_trials(page, limit)))
         return get_all_trials(page, limit)
